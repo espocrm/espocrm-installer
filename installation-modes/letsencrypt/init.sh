fb616dd398b5a928acb68e825a98a03aeed94fa1
Taras Machyshyn <taras@espocrm.com>
2021-06-04 18:02:32 +0300

    Fixed letsencrypt method

--- installation-modes/letsencrypt/init.sh
+++ installation-modes/letsencrypt/init.sh
@@ -20,12 +20,12 @@ prepareConfiguration
 
 # Let's Encrypt
 
-mkdir -p ./ssl \
-    ./tmp \
-    ./certbot
+mkdir -p ./nginx/ssl \
+    ./nginx/certbot \
+    ./tmp
 
-curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "ssl/ssl-options.conf"
-curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "ssl/dhparams.pem"
+curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "./nginx/ssl/ssl-options.conf"
+curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "./nginx/ssl/dhparams.pem"
 
 # Run templorary nginx
 echo "server {
@@ -46,15 +46,15 @@ docker container inspect tmp-nginx > /dev/null 2>&1 && docker rm -f tmp-nginx
 # Run tmp-nginx
 docker run --name tmp-nginx \
     -v $scriptDirectory/tmp/nginx-default.conf:/etc/nginx/conf.d/default.conf \
-    -v $scriptDirectory/ssl:/etc/letsencrypt \
-    -v $scriptDirectory/certbot:/var/www/certbot \
+    -v $scriptDirectory/nginx/ssl:/etc/letsencrypt \
+    -v $scriptDirectory/nginx/certbot:/var/www/certbot \
     -p 80:80 \
     -d nginx
 
 # Generate certificates
 docker run -it --rm \
-    -v $scriptDirectory/ssl:/etc/letsencrypt \
-    -v $scriptDirectory/certbot:/var/www/certbot \
+    -v $scriptDirectory/nginx/ssl:/etc/letsencrypt \
+    -v $scriptDirectory/nginx/certbot:/var/www/certbot \
     certbot/certbot \
     certonly --webroot \
     -w /var/www/certbot \
@@ -70,7 +70,7 @@ docker stop tmp-nginx
 docker rm tmp-nginx
 rm -rf ./tmp
 
-if [ ! -d "./ssl/live/$domain" ]; then
+if [ ! -d "./nginx/ssl/live/$domain" ]; then
     echo "Error: Failed to create Let's Encrypt certificate. Please fix errors described above and try again."
     exit 1
 fi
