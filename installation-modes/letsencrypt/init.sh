228cad9d50e983d9f5fdbf680658df0296c12e00
Taras Machyshyn <taras@espocrm.com>
2021-06-04 18:20:53 +0300

    Mode 2 fixes

--- installation-modes/letsencrypt/init.sh
+++ installation-modes/letsencrypt/init.sh
@@ -20,12 +20,12 @@ prepareConfiguration
 
 # Let's Encrypt
 
-mkdir -p ./nginx/ssl \
-    ./nginx/certbot \
+mkdir -p "./$server/ssl" \
+    "./$server/certbot" \
     ./tmp
 
-curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "./nginx/ssl/ssl-options.conf"
-curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "./nginx/ssl/dhparams.pem"
+curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "./$server/ssl/ssl-options.conf"
+curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "./$server/ssl/dhparams.pem"
 
 # Run templorary nginx
 echo "server {
@@ -45,16 +45,16 @@ docker container inspect tmp-nginx > /dev/null 2>&1 && docker rm -f tmp-nginx
 
 # Run tmp-nginx
 docker run --name tmp-nginx \
-    -v $scriptDirectory/tmp/nginx-default.conf:/etc/nginx/conf.d/default.conf \
-    -v $scriptDirectory/nginx/ssl:/etc/letsencrypt \
-    -v $scriptDirectory/nginx/certbot:/var/www/certbot \
+    -v "$scriptDirectory/tmp/nginx-default.conf":/etc/nginx/conf.d/default.conf \
+    -v "$scriptDirectory/$server/ssl":/etc/letsencrypt \
+    -v "$scriptDirectory/$server/certbot":/var/www/certbot \
     -p 80:80 \
     -d nginx
 
 # Generate certificates
 docker run -it --rm \
-    -v $scriptDirectory/nginx/ssl:/etc/letsencrypt \
-    -v $scriptDirectory/nginx/certbot:/var/www/certbot \
+    -v "$scriptDirectory/$server/ssl":/etc/letsencrypt \
+    -v "$scriptDirectory/$server/certbot":/var/www/certbot \
     certbot/certbot \
     certonly --webroot \
     -w /var/www/certbot \
@@ -63,14 +63,13 @@ docker run -it --rm \
     --email $email \
     --rsa-key-size 4096 \
     --force-renewal \
-    --staging \
     -d $domain
 
 docker stop tmp-nginx
 docker rm tmp-nginx
 rm -rf ./tmp
 
-if [ ! -d "./nginx/ssl/live/$domain" ]; then
+if [ ! -d "./$server/ssl/live/$domain" ]; then
     echo "Error: Failed to create Let's Encrypt certificate. Please fix errors described above and try again."
     exit 1
 fi
