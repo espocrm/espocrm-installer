6d167e71df6192bde8304fee58ee6539c47bbb7b
Taras Machyshyn <taras@espocrm.com>
2023-07-24 16:09:54 +0300

    Year change

--- commands/command.sh
+++ commands/command.sh
@@ -3,7 +3,7 @@
 # EspoCRM installer MASTER
 #
 # EspoCRM - Open Source CRM application.
-# Copyright (C) 2014-2022 Yurii Kuznietsov, Taras Machyshyn, Oleksii Avramenko
+# Copyright (C) 2014-2023 Yurii Kuznietsov, Taras Machyshyn, Oleksii Avramenko
 # Website: https://www.espocrm.com
 
 set -e
